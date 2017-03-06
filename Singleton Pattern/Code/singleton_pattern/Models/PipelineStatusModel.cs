2ff6cfe483ecefd0198fa2190b7a46779ed90007
Frank.Spencer <c.tait@outlook.com>
2017-03-06 21:02:52 +0000

    Adding the Signleton Pattern

---
+++ Singleton Pattern/Code/singleton_pattern/Models/PipelineStatusModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace singleton_pattern.Models
+{
+    public class PipelineStatusModel
+    {
+        public string PausedCause { get; set; }
+        public string PausedBy { get; set; }
+        public Boolean Paused { get; set; }
+        public Boolean Schedulable { get; set; }
+        public Boolean Locked { get; set; }
+    }
+}
---
+++ Singleton Pattern/Code/singleton_pattern/Repository/HttpRepository.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using singleton_pattern.Models;
+
+namespace singleton_pattern.Repository
+{
+    public class HttpRepository
+    {
+        HttpClient Client = HttpClientSingleton.Instance();
+        public PipelineStatusModel HttpGetPipelineStatus(string PipelineName)
+        {
+            Task<string> Response = Client.GetStringAsync(string.Format("http://localhost:8153/go/api/pipelines/{0}/status", PipelineName));
+            PipelineStatusModel PipelineStatusObject = JsonConvert.DeserializeObject<PipelineStatusModel>(Response.Result);
+            return PipelineStatusObject;
+        }
+    }
+}
